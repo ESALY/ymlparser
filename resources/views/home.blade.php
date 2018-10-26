419b6819641bfdad3e3f500fd771756da380a1e8
Ruslan Akhmetov <slan.ru@gmail.com>
2018-10-26 15:59:24 +0300

    deploy setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="app">
+        @{{hello}}
+    </div>
+    {{--<script src="{{ asset('/js/app.js') }}"></script>--}}
+
+    <script>
+        axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+        // bootstrap the demo
+        var vapp = new Vue({
+            el: '#app',
+            data: {
+                hello: 'im vue',
+            },
+            methods:{}
+            ,
+            created: function(){
+            }
+        });
+
+
+    </script>
+
+@endsection
