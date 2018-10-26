def34649db88c70d73340c03cc9f2e8a9c7a3dd6
Ruslan Akhmetov <slan.ru@gmail.com>
2018-10-26 20:25:30 +0300

    Revert "make it better2"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,6 @@
     <!-- Js -->
     <script src="{{ asset('/js/vue.js') }}"></script>
     <script src="{{ asset('/js/axios.min.js') }}"></script>
-7977777
 
     <!-- import JavaScript -->
     <script src="https://unpkg.com/element-ui/lib/index.js"></script>
