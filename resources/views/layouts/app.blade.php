ff062b3452d25790813e107eef64306bbbd6f407
Ruslan Akhmetov <slan.ru@gmail.com>
2018-10-26 19:50:00 +0300

    Revert "make it better"
    This reverts commit d80d53b

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="shortcut icon" href="images/favicon.png">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+
+
+    <!-- Js -->
+    <script src="{{ asset('/distrib/vue.js') }}"></script>
+    <script src="{{ asset('/distrib/axios.min.js') }}"></script>
+
+
+    <!-- import JavaScript -->
+    <script src="https://unpkg.com/element-ui/lib/index.js"></script>
+    <script src="https://unpkg.com/element-ui/lib/umd/locale/ru-RU.js"></script>
+
+    <script>
+        ELEMENT.locale(ELEMENT.lang.ruRU)
+    </script>
+
+</head>
+<body>
+
+@yield('content')
+
+</body>
+
+</html>
