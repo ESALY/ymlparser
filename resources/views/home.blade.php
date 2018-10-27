1f66ac7f8820e878741028b42c1134cbb2c34fd4
Ruslan Akhmetov <slan.ru@gmail.com>
2018-10-28 01:56:08 +0300

    small changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,21 +24,11 @@
                     <template>
                         <el-table
                                 :data="products"
-                                height="250"
                                 style="width: 100%">
-                            <el-table-column
-                                    prop="date"
-                                    label="Date"
-                                    width="180">
-                            </el-table-column>
                             <el-table-column
                                     prop="name"
                                     label="Name"
                                     width="180">
-                            </el-table-column>
-                            <el-table-column
-                                    prop="address"
-                                    label="Address">
                             </el-table-column>
                         </el-table>
                     </template>
