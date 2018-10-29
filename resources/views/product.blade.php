f154481932e7f92692a162357178194a1234280b
Ruslan Akhmetov <slan.ru@gmail.com>
2018-10-29 03:54:16 +0300

    small changes

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -10,21 +10,8 @@
                 <el-header>
                     {{--Header--}}
                     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-                        <el-menu-item index="1">Processing Center</el-menu-item>
-                        <el-submenu index="2">
-                            <template slot="title">Workspace</template>
-                            <el-menu-item index="2-1">item one</el-menu-item>
-                            <el-menu-item index="2-2">item two</el-menu-item>
-                            <el-menu-item index="2-3">item three</el-menu-item>
-                            <el-submenu index="2-4">
-                                <template slot="title">item four</template>
-                                <el-menu-item index="2-4-1">item one</el-menu-item>
-                                <el-menu-item index="2-4-2">item two</el-menu-item>
-                                <el-menu-item index="2-4-3">item three</el-menu-item>
-                            </el-submenu>
-                        </el-submenu>
-                        <el-menu-item index="3" disabled>Info</el-menu-item>
-                        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
+                        <el-menu-item index="1"><a href="{{URL::to('/')}}" target="_blank">Products</a></el-menu-item>
+                        <el-menu-item index="2"><a href="{{URL::to('/import')}}" target="_blank">Import</a></el-menu-item>
                     </el-menu>
                 </el-header>
                 <el-main>
