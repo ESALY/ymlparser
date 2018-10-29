6ade6722b11307b8a8821ba99a429d0080fcd012
Ruslan Akhmetov <slan.ru@gmail.com>
2018-10-29 03:30:41 +0300

    small changes

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,73 +2,24 @@
 
 @section('content')
     <div id="app">
-    <el-container  :style="{ height: window.height + 'px' }">
-        <el-aside width="300px">
-            {{--Aside--}}
-            <div class="search-wrapper">
-                <el-form :label-position="searchForm.labelPosition" label-width="100px" :model="searchForm">
-                    <el-form-item label="Поиск">
-                        <el-input v-model="searchForm.name" @change="getItems()" clearable></el-input>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="getItems()">Искать</el-button>
-                    </el-form-item>
-                </el-form>
-            </div>
-        </el-aside>
-        <el-container>
-            <el-header>Header</el-header>
-            <el-main>
-                <div  class="products">
-                    <template>
-                        <el-table
-                                :data="products"
-                                style="width: 100%">
-                            {{--<el-table-column
-                                    fixed
-                                    label="Img"
-                                    width="80">
-                                <template slot-scope="scope">
-                                    <a :href="'https://www.vk.com/id' + scope.row.raw_id" target="_blank">
-                                        <div class="image-wrap" v-bind:style="{ backgroundImage: 'url(' + scope.row.img + ')' }"></div>
-                                    </a>
-                                </template>
-                            </el-table-column>--}}
-                            <el-table-column
-                                    label="Name"
-                                    width="350">
-                            <template slot-scope="scope">
-                                <a :href="'/id/' + scope.row.raw_id" target="_blank">
-                                    @{{ scope.row.name }}
-                                </a>
-                            </template>
-                            </el-table-column>
-                            <el-table-column
-                                    label="Category"
-                                    width="350">
-                                <template slot-scope="scope">
-                                    @{{ scope.row. cat_name }}
-                                </template>
-                            </el-table-column>
+        <el-container  :style="{ height: window.height + 'px' }">
+            <el-aside width="300px">
+                {{--Aside--}}
+            </el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>
+                    <div  class="product">
 
-                            <el-table-column
-                                    label="Price"
-                                    width="">
-                                <template slot-scope="scope">
-                                        @{{ scope.row.price }}
-                                </template>
-                            </el-table-column>
-                        </el-table>
-                    </template>
-                </div>
-            </el-main>
-            <el-footer>
-                {{--Footer--}}
-                Width: @{{ window.width }},
-                Height: @{{ window.height }}
-            </el-footer>
+                    </div>
+                </el-main>
+                <el-footer>
+                    {{--Footer--}}
+                    Width: @{{ window.width }},
+                    Height: @{{ window.height }}
+                </el-footer>
+            </el-container>
         </el-container>
-    </el-container>
     </div>
     </div>
     <script>
@@ -93,10 +44,10 @@
             },
             methods: {
                 init: function () {
-                    //this.getItems();
+                    this.getItems();
                 },
                 showProduct: function () {
-                   console.log(12);
+                    console.log(12);
                 },
                 getItems: function () {
 
